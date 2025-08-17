c63445ee93327426651e9ad58c6da16b35fd3ad1
Yasser Naji <yfnaji@gmail.com>
2025-08-17 21:37:31 +0100

    Cubic-Splines: Modifications to InterpolationIndex and InterpolationValue traits + introduce the IntoValue trait

--- crates/RustQuant_math/src/interpolation/mod.rs
+++ crates/RustQuant_math/src/interpolation/mod.rs
@@ -7,7 +7,7 @@
 //      - LICENSE-MIT.md
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-use std::ops::{Div, Mul, Sub, AddAssign};
+use std::ops::{Div, Mul, MulAssign, Sub, Add, AddAssign, Neg};
 use RustQuant_error::RustQuantError;
 
 pub mod linear_interpolator;
@@ -19,11 +19,30 @@ pub use exponential_interpolator::*;
 pub mod b_splines;
 pub use b_splines::*;
 
+pub mod cubic_spline;
+pub use cubic_spline::*;
+
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 /// Trait describing requirements to be interpolated.
-pub trait InterpolationValue: num::Num + AddAssign + std::fmt::Debug + Copy + Clone + Sized {}
+pub trait InterpolationValue: num::Num 
+    + num::FromPrimitive 
+    + Neg<Output = Self> 
+    + AddAssign
+    + MulAssign
+    + Copy
+    + Clone
+    + Sized
+    + std::fmt::Display
+    + std::fmt::Debug {}
+
+/// Trait to convert a Delta type into a value of `ValueType`.
+pub trait IntoValue<ValueType> {
+    /// Convert `self` into `ValueType`.
+    fn into_value(self) -> ValueType;
+}
 
 /// Trait describing requirements to be an index of interpolation.
 pub trait InterpolationIndex:
@@ -31,7 +50,11 @@ pub trait InterpolationIndex:
 {
     /// Type of the difference of `Self` - `Self`
     type Delta: Div<Self::Delta, Output = Self::DeltaDiv>
-        + Mul<Self::DeltaDiv, Output = Self::Delta>;
+        + Mul<Self::DeltaDiv, Output = Self::Delta>
+        + Add<Self::Delta, Output = Self::Delta>
+        + Sub<Self::Delta, Output = Self::Delta>
+        + IntoValue<Self::DeltaDiv>
+        + Copy;
 
     /// Type of `Delta` / `Delta`
     type DeltaDiv: InterpolationValue;
@@ -63,7 +86,16 @@ where
     fn add_point(&mut self, point: (IndexType, ValueType));
 }
 
-impl<T> InterpolationValue for T where T: num::Num + AddAssign + std::fmt::Debug + Copy + Clone + Sized {}
+impl<T> InterpolationValue for T where T: num::Num 
+    + num::FromPrimitive 
+    + Neg<Output = Self> 
+    + AddAssign
+    + MulAssign
+    + Copy
+    + Clone
+    + Sized
+    + std::fmt::Display
+    + std::fmt::Debug {}
 
 macro_rules! impl_interpolation_index {
     ($a:ty, $b:ty, $c:ty) => {
@@ -74,31 +106,52 @@ macro_rules! impl_interpolation_index {
     };
 }
 
+macro_rules! impl_num_delta_into_value {
+    ($b:ty, $c:ty) => {
+        impl IntoValue<$c> for $b {
+            fn into_value(self) -> $c {
+                self as $c
+            }
+        }   
+    };
+}
+
+macro_rules! impl_time_delta_into_value {
+    ($b:ty, $c:ty) => {
+        impl IntoValue<$c> for $b {
+            fn into_value(self) -> $c {
+                self.as_seconds_f64()
+            }
+        }
+    };
+}
+
 // Implement InterpolationIndex for all signed integer types.
 impl_interpolation_index!(i8, i8, i8);
+impl_num_delta_into_value!(i8, i8);
 impl_interpolation_index!(i16, i16, i16);
+impl_num_delta_into_value!(i16, i16);
 impl_interpolation_index!(i32, i32, i32);
+impl_num_delta_into_value!(i32, i32);
 impl_interpolation_index!(i64, i64, i64);
+impl_num_delta_into_value!(i64, i64);
 impl_interpolation_index!(i128, i128, i128);
+impl_num_delta_into_value!(i128, i128);
 impl_interpolation_index!(isize, isize, isize);
-
-// Implement InterpolationIndex for all unsigned integer types.
-impl_interpolation_index!(u8, u8, u8);
-impl_interpolation_index!(u16, u16, u16);
-impl_interpolation_index!(u32, u32, u32);
-impl_interpolation_index!(u64, u64, u64);
-impl_interpolation_index!(u128, u128, u128);
-impl_interpolation_index!(usize, usize, usize);
+impl_num_delta_into_value!(isize, isize);
 
 // Implement InterpolationIndex for all floating point types.
 impl_interpolation_index!(f32, f32, f32);
+impl_num_delta_into_value!(f32, f32);
 impl_interpolation_index!(f64, f64, f64);
+impl_num_delta_into_value!(f64, f64);
 
 // Implement InterpolationIndex for date/time types.
 impl_interpolation_index!(time::Date, time::Duration, f64);
 impl_interpolation_index!(time::Time, time::Duration, f64);
 impl_interpolation_index!(time::OffsetDateTime, time::Duration, f64);
 impl_interpolation_index!(time::PrimitiveDateTime, time::Duration, f64);
+impl_time_delta_into_value!(time::Duration, f64);
 
 // Implement InterpolationIndex for Decimal type.
 impl_interpolation_index!(
@@ -106,3 +159,4 @@ impl_interpolation_index!(
     rust_decimal::Decimal,
     rust_decimal::Decimal
 );
+impl_num_delta_into_value!(rust_decimal::Decimal, rust_decimal::Decimal);
